e013b8e580709bb9f5445f7159ad878346af53d9
Honza Javorek <mail@honzajavorek.cz>
2024-04-17 11:20:35 +0200

    move functions to actors

--- jg/plucker/actors.py
+++ jg/plucker/actors.py
@@ -1,14 +1,22 @@
 from pathlib import Path
-from typing import Generator, Type
+from typing import Any, Generator, Type
 
 import nest_asyncio
 from apify import Actor, Configuration
 from apify.scrapy.utils import apply_apify_settings
 from scrapy import Item, Spider
+from scrapy.crawler import CrawlerProcess
 from scrapy.settings import BaseSettings, Settings
 from scrapy.spiderloader import SpiderLoader as BaseSpiderLoader
+from scrapy.statscollectors import StatsCollector
 
-from jg.plucker.spiders import run_spider
+
+def run_spider(settings: Settings, spider_class: type[Spider]):
+    crawler_process = CrawlerProcess(settings, install_root_handler=False)
+    crawler_process.crawl(spider_class)
+    stats_collector = get_stats_collector(crawler_process)
+    crawler_process.start()
+    raise_for_stats(stats_collector.get_stats())
 
 
 async def run_actor(settings: Settings, spider_class: Type[Spider]) -> None:
@@ -74,3 +82,30 @@ def generate_schema(item_class: Type[Item]) -> dict:
             }
         },
     }
+
+
+def get_stats_collector(crawler_process: CrawlerProcess) -> StatsCollector:
+    assert len(crawler_process.crawlers) == 1, "Exactly one crawler expected"
+    crawler = crawler_process.crawlers.pop()
+    return crawler.stats
+
+
+class StatsError(RuntimeError):
+    pass
+
+
+def raise_for_stats(stats: dict[str, Any]):
+    item_count = stats.get("item_scraped_count", 0)
+    if exc_count := stats.get("spider_exceptions"):
+        raise StatsError(f"Exceptions raised: {exc_count}")
+    if critical_count := stats.get("log_count/CRITICAL"):
+        raise StatsError(f"Critical errors logged: {critical_count}")
+    if error_count := stats.get("log_count/ERROR"):
+        raise StatsError(f"Errors logged: {error_count}")
+    if item_count < 10:
+        raise StatsError(f"Few items scraped: {item_count}")
+    if reason := stats.get("finish_reason"):
+        if reason != "finished":
+            raise StatsError(f"Scraping finished with reason {reason!r}")
+    if item_count := stats.get("item_dropped_reasons_count/MissingRequiredFields"):
+        raise StatsError(f"Items missing required fields: {item_count}")
