3e623b3f8f582c6d544af176e22c18bb12fb3d0c
Honza Javorek <mail@honzajavorek.cz>
2024-10-24 15:58:43 +0200

    try if we can go faster

--- jg/plucker/job_checks/spider.py
+++ jg/plucker/job_checks/spider.py
@@ -32,7 +32,7 @@ class Spider(BaseSpider):
     custom_settings = {
         "HTTPERROR_ALLOWED_CODES": [404, 410],
         "CONCURRENT_REQUESTS_PER_DOMAIN": 2,
-        "DOWNLOAD_SLOTS": {"www.linkedin.com": {"concurrency": 1, "delay": 1}},
+        # "DOWNLOAD_SLOTS": {"www.linkedin.com": {"concurrency": 1, "delay": 1}},
         "RETRY_TIMES": 10,
         "DUPEFILTER_CLASS": "scrapy.dupefilters.BaseDupeFilter",
         "METAREFRESH_ENABLED": False,
