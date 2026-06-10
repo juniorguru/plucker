18d2f8242885a1ee2751dba24cc9c89eb020859b
Honza Javorek <mail@honzajavorek.cz>
2026-06-10 11:25:53 +0200

    test behavior

--- tests/followers/test_spider.py
+++ tests/followers/test_spider.py
@@ -4,10 +4,23 @@ from pathlib import Path
 import pytest
 from scrapy.http.response.html import HtmlResponse
 
-from jg.plucker.followers.spider import Spider
+from jg.plucker.followers.spider import Spider, get_domain
 
 
 FIXTURES_DIR = Path(__file__).parent
+
+
+@pytest.mark.parametrize(
+    "url, expected_domain",
+    [
+        ("https://www.instagram.com/juniordotguru", "instagram.com"),
+        ("https://www.linkedin.com/posts/some-activity", "linkedin.com"),
+        ("https://mastodonczech.cz/@honzajavorek", "mastodonczech.cz"),
+        ("https://example.com/facebook.html", "example.com"),
+    ],
+)
+def test_get_domain(url: str, expected_domain: str):
+    assert get_domain(url) == expected_domain
 
 
 def test_spider_parse_mastodon():
@@ -39,7 +52,7 @@ def test_spider_parse_linkedin(filename: str, expected_count: int):
         body=Path(FIXTURES_DIR / filename).read_bytes(),
     )
     spider = Spider()
-    item = spider.parse_linkedin(response, today=date(2025, 3, 18))
+    item = spider.parse_linkedin(response, today=date(2025, 3, 18), name="linkedin")
 
     assert item == {
         "date": date(2025, 3, 18),
@@ -66,69 +79,26 @@ def test_spider_parse_linkedin_personal():
     }
 
 
-def test_spider_parse_facebook():
-    filename = "facebook.html"
+@pytest.mark.parametrize(
+    "filename, expected_count",
+    [
+        ("facebook.html", 413),
+        ("facebook_personal.html", 668),
+        ("instagram.html", 652),
+        ("instagram_personal.html", 329),
+    ],
+)
+def test_spider_parse_meta(filename: str, expected_count: int):
     response = HtmlResponse(
         f"https://example.com/{filename}",
         body=Path(FIXTURES_DIR / filename).read_bytes(),
     )
     spider = Spider()
-    item = spider.parse_facebook(response, today=date(2025, 3, 18))
+    name = filename.removesuffix(".html")
+    item = spider.parse_meta(response=response, today=date(2025, 3, 18), name=name)
 
     assert item == {
         "date": date(2025, 3, 18),
-        "name": "facebook",
-        "count": 413,
-    }
-
-
-def test_spider_parse_facebook_personal():
-    filename = "facebook_personal.html"
-    response = HtmlResponse(
-        f"https://example.com/{filename}",
-        body=Path(FIXTURES_DIR / filename).read_bytes(),
-    )
-    spider = Spider()
-    item = spider.parse_facebook(
-        response, today=date(2025, 3, 18), name="facebook_personal"
-    )
-
-    assert item == {
-        "date": date(2025, 3, 18),
-        "name": "facebook_personal",
-        "count": 668,
-    }
-
-
-def test_spider_parse_instagram():
-    filename = "instagram.html"
-    response = HtmlResponse(
-        f"https://example.com/{filename}",
-        body=Path(FIXTURES_DIR / filename).read_bytes(),
-    )
-    spider = Spider()
-    item = spider.parse_instagram(response, today=date(2025, 3, 18))
-
-    assert item == {
-        "date": date(2025, 3, 18),
-        "name": "instagram",
-        "count": 652,
-    }
-
-
-def test_spider_parse_instagram_personal():
-    filename = "instagram_personal.html"
-    response = HtmlResponse(
-        f"https://example.com/{filename}",
-        body=Path(FIXTURES_DIR / filename).read_bytes(),
-    )
-    spider = Spider()
-    item = spider.parse_instagram(
-        response, today=date(2025, 3, 18), name="instagram_personal"
-    )
-
-    assert item == {
-        "date": date(2025, 3, 18),
-        "name": "instagram_personal",
-        "count": 328,
+        "name": name,
+        "count": expected_count,
     }
